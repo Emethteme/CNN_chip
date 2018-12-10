2b3de116159ac1dad40ddb4d2bad99ecfd41303f
Emethteme <Emethteme@gmail.com>
2018-12-10 15:47:37 +0800

    Add files via upload

---
+++ 06_POST_SIM/01_run.f
@@ -0,0 +1,7 @@
+ncverilog TESTBED.v \
+-v /misc/RAID2/COURSE/iclab/iclabta01/umc018/Verilog/umc18io3v5v.v \
+-v /misc/RAID2/COURSE/iclab/iclabta01/umc018/Verilog/umc18_neg.v \
+-v ../04_MEM/RA1SH.v \
++define+POSIM +access+wr \
++maxdelays\
+ +incdir+/usr/syn/dw/sim_ver
---
+++ 06_POST_SIM/INCA_libs/irun.lnx86.13.10.nc/files.ts
@@ -0,0 +1,4 @@
+1511484384 /misc/RAID2/COURSE/iclab/iclabta01/umc018/Verilog/umc18io3v5v.v
+1511484384 /misc/RAID2/COURSE/iclab/iclabta01/umc018/Verilog/umc18_neg.v
+1514983892 /misc/RAID2/COURSE/iclab/iclab39/FP/04_MEM/RA1SH.v
+1513846701 /misc/RAID2/COURSE/iclab/iclab39/FP/00_TESTBED/TESTBED.v
